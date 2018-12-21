3df03a723ed55c2b151e9c20ab7a1cd44fdb324a
LolyaDi <winx55@list.ru>
2018-12-22 00:29:08 +0600

    Создан репозиторий с дзшкой

---
+++ FirstDZ/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstDZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int POWER = 5;
+
+            Console.Write("Введите любое число: ");
+            string str = Console.ReadLine();
+
+            int number = Convert.ToInt32(str);
+
+            Console.WriteLine($"Вывод числа в пятой степени: {Math.Pow(number, POWER)}");
+
+            Console.ReadLine();
+        }
+    }
+}
+
